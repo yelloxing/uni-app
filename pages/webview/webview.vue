420f9c1f5e7bd0347b2fc4a2ab3b4731a00cef3d
心叶 <yelloxing@gmail.com>
2020-12-16 09:49:22 +0800

    测试webview

---
+++ pages/webview/webview.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<web-view src='https://hai2007.gitee.io/clunch/'></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
