41054b8f6af89c119d74d3e5bf2f4baec1e67c1e
心叶 <yelloxing@gmail.com>
2020-12-21 17:42:57 +0800

    添加路由跳转

---
+++ pages/AboutUS-Detail/AboutUS-Detail.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		关于我们的详情页面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- pages/AboutUS/AboutUS.vue
+++ pages/AboutUS/AboutUS.vue
@@ -1,6 +1,11 @@
 <template>
 	<view>
 		关于我们
+		<navigator url="../AboutUS-Detail/AboutUS-Detail">
+			<button type="primary">
+				点击查看具体内容
+			</button>
+		</navigator>
 	</view>
 </template>
 
@@ -8,11 +13,11 @@
 	export default {
 		data() {
 			return {
-				
+
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
