40162b3c04d2c52210dc02c64733f2ecf9e9c086
心叶 <yelloxing@gmail.com>
2020-12-21 18:14:56 +0800

    clunch在uniapp中测试

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,10 +1,15 @@
 <template>
 	<view>
-		主页
+		<view>
+			测试clunch如何在uni-app中进行支持
+		</view>
+		<view id='canvas' style="width: 750rpx; height: 200px;">
+		</view>
 	</view>
 </template>
 
 <script>
+	import Clunch from 'clunch/dist/clunch-template.js';
 	export default {
 		data() {
 			return {
@@ -13,6 +18,15 @@
 		},
 		methods: {
 
+		},
+		mounted() {
+			// let query=uni.createSelectorQuery();
+			// let canvas=query.select('#canvas');
+			// debugger
+			// new Clunch({
+			// 	el:canvas,
+			// 	template:"<circle :cx='_width*0.5' cy='50' radius='50' />"
+			// });
 		}
 	}
 </script>
